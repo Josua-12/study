e09194e6c8fb715cca2ff741a8a7d2cb461a58a3
jsua86268 <jsua86268@gmail.com>
2025-07-15 15:20:59 +0900

    0715

---
+++ Notes/etc/src/jdk.jdeps/com/sun/tools/jdeps/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.jdeps.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "17.0.15+9-LTS-241" },
+            { "jdk", "17.0.15" },
+            { "release", "17.0.15" },
+        };
+    }
+}
