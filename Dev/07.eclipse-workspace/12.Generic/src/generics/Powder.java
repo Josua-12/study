f41cdf9c8c9f80ba9be50eadae1be47746162644
jsua86268 <jsua86268@gmail.com>
2025-07-23 15:26:50 +0900

    0723

---
+++ Dev/07.eclipse-workspace/12.Generic/src/generics/Powder.java
@@ -0,0 +1,14 @@
+package generics;
+
+public class Powder extends Material {
+	
+	// @Overriding이 없어도 이름이 똑같으면 오버라이딩 된 것임
+	public void doPrinting() {
+		System.out.println("Powder 재료로 출력합니다.");
+	}
+	
+	@Override
+	public String toString() {
+		return "재료는 Powder입니다.";
+	}
+}
