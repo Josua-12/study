8997af347fd4b37800487ca0472551b8c0f66c0f
jsua86268 <jsua86268@gmail.com>
2025-07-31 17:50:38 +0900

    0731

--- Dev/07.eclipse-workspace/18.JavaIO/src/inputstream/FileInputStreamTest2.java
+++ Dev/07.eclipse-workspace/18.JavaIO/src/inputstream/FileInputStreamTest2.java
@@ -7,7 +7,7 @@ public class FileInputStreamTest2 {
 	
 	public static void main(String[] args) {
 		
-		try(FileInputStream fis = new FileInputStream("input.txt")) {
+		try(FileInputStream fis = new FileInputStream("reader.txt")) {
 			
 			int i;
 			// i값이 -1이 아닌 동안 read() 메서드로 한 바이트를 반복해 읽음
